743edf8fdb1a0311d6e8dc21e0419c38c0671315
shenjian2669647 <554071767@qq.com>
2022-05-31 10:07:04 +0800

    初始化vuetify项目

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        width="200px"
+        app
+    >
+      <v-list>
+        <v-list-item
+            v-for="[icon, text] in links"
+            :key="icon"
+            link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    title:'首页',
+    links: [
+      ['mdi-inbox-arrow-down', 'Inbox'],
+      ['mdi-send', 'Send'],
+      ['mdi-delete', 'Trash'],
+      ['mdi-alert-octagon', 'Spam'],
+    ],
+  }),
+}
+</script>
